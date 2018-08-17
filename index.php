ec9c2aadc520a8264f929ca2598e439e7d8113cf
Jack Collins <jackcollins434@gmail.com>
2018-08-16 17:42:46 -0700

    updated version

--- index.php
+++ index.php
@@ -5,13 +5,17 @@ require 'bootstrap.php';
 $records = Record::all()->toArray();
 
 foreach ($records as $record) {
-    $newRecord = NewRecord::where('email', $record['email'])
+    if ($record['email'] === null && $record['phone'] === null) {
+
+    } else {
+        $newRecord = NewRecord::where('email', $record['email'])
         ->orWhere('phone', $record['phone'])
         ->first();
 
-    if ($newRecord === null) {
-        (new NewRecord)->fill($record)->save();
-    } else {
-        $newRecord->fill($record)->update();
+        if ($newRecord === null) {
+            (new NewRecord)->fill($record)->save();
+        } else {
+            $newRecord->fill($record)->update();
+        }
     }
 }
